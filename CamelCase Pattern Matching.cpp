f18a810793a30c7188c7858189bb5faeaf58995b
Sagar ray <raysagar807@gmail.com>
2023-05-29 22:05:16 +0530

    29th of may 2023

---
+++ CamelCase Pattern Matching.cpp
@@ -0,0 +1,48 @@
+
+#include <algorithm>
+#include <cstring>
+class Solution {
+  public:
+    vector<string> CamelCase(int n, vector<string> d, string p) {
+       
+       vector<string> ans;
+       
+       for(auto it:d)
+       {
+           int i=0;
+           bool flag=1;
+           for(auto x:it)
+           {
+               if(i>=p.size())
+               break;
+               
+               if(x>='A' and x<='Z')
+               {
+                   if(x!=p[i])
+                   {
+                       flag=0;
+                       break;
+                   }
+                   else
+                   i++;
+               }
+           }
+           
+           if(flag and i>=p.size())
+           ans.push_back(it);
+       }
+       
+       if(ans.size()>=1)
+       sort(ans.begin(),ans.end());
+       else
+       ans.push_back("-1");
+       
+       return ans;
+    }
+};
+/* time complexity - O(n*S)
+   space complexity - O(n*S)
+   
+   where n is size of a string aaray
+         S is a size of a string 
+*/
