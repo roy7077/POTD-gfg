ae22fbeb7001aa587cc147e28ed1322ba58675a2
Sagar ray <raysagar807@gmail.com>
2023-07-17 01:04:55 +0530

    17th of july 2023

---
+++ First non-repeating character in a stream.cpp
@@ -0,0 +1,26 @@
+class Solution {
+	public:
+		string FirstNonRepeating(string A){
+		    // Code here
+		    queue<int> q;
+		    int arr[26]={0};
+		    
+		    for(int i=0;i<A.size();i++)
+		    {
+		        arr[A[i]-'a']++;
+		        if(arr[A[i]-'a']==1){
+		            q.push(A[i]);
+		        }
+		        while(!q.empty() && arr[q.front()-'a']!=1){
+		            q.pop();
+		        }
+		        
+		        if(q.empty()){
+		            A[i]='#';
+		        }else{
+		            A[i]=q.front();
+		        }
+		    }
+		    return A;
+		}
+};
