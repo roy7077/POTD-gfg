1a2fb4bc8499e8e8929899d5b3eaea14e68a2b53
Sagar ray <raysagar807@gmail.com>
2023-10-21 20:17:14 +0530

    21st of oct 2023 easy

---
+++ Sum of all divisors from 1 to n.cpp
@@ -0,0 +1,12 @@
+class Solution
+{
+    public:
+    long long sumOfDivisors(int N)
+    {
+        long long sum=0;
+        for(int i=1;i<=N;i++)
+        sum+=(N/i)*i;
+        
+        return sum;
+    }
+};
