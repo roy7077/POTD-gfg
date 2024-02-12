c94afccfc08aa40fe8948f19eded7f7847f8a859
Sagar ray <raysagar807@gmail.com>
2024-02-12 23:59:58 +0530

    12th of feb 2024 easy

--- Recursive sequence.cpp
+++ Recursive sequence.cpp
@@ -1,4 +1,6 @@
 
+//CPP
+
 /* Method - 1
    Iterative
    Time complexity - O(N^2)
@@ -56,3 +58,97 @@ class Solution{
         return help(n,1,last);
     }
 };
+ 
+
+//JAVA
+
+class Solution{
+    static int mod=1000000007;
+    static long sequence(int n){
+        long ans=0;
+        long pre=1;
+        for(int i=1;i<=n;i++)
+        {
+            long temp=1;
+            for(int j=1;j<=i;j++)
+            {
+                temp=(temp*pre)%mod;
+                pre++;
+            }
+            ans=(ans+temp)%mod;
+        }
+        return ans;
+    }
+}
+
+class Solution{
+    static int mod=1000000007;
+    static long help(int n,int i,int last)
+    {
+        //base case
+        if(i>n)
+        return 0;
+        
+        //recursive calls
+        //and small calculation
+        long temp=1;
+        for(int j=1;j<=i;j++)
+        {
+            temp=(temp*last)%mod;
+            last++;
+        }
+        
+        return (temp+help(n,i+1,last))%mod;
+    }
+    static long sequence(int n){
+        return help(n,1,1);
+    }
+}
+ 
+
+//JAVASCRIPT
+
+class Solution {
+    sequence(n)
+    {
+        let mod=1000000007;
+        let ans=0;
+        let pre=1;
+        for(let i=1;i<=n;i++)
+        {
+            let temp=1;
+            for(let j=1;j<=i;j++)
+            {
+                temp=(temp*pre)%mod;
+                pre++;
+            }
+            ans=(ans+temp)%mod;
+        }
+        return ans;
+    }
+}
+
+// RECURSION
+class Solution {
+    mod = 1000000007;
+    help(n,i,last)
+    {
+        //base case
+        if(i>n)
+        return 0;
+        
+        //recursive calls
+        //and small calculation
+        let temp=1;
+        for(let j=1;j<=i;j++)
+        {
+            temp=(temp*last)%this.mod;
+            last++;
+        }
+        
+        return (temp+this.help(n,i+1,last))%this.mod;
+    }
+    sequence(n) {
+        return this.help(n,1,1);
+    }
+}
