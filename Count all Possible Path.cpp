3fbca2e602dfaf7c70ed2bb0b3c4c3317ce44333
Sagar ray <raysagar807@gmail.com>
2024-02-15 21:12:05 +0530

    15th of feb 2024 medium

---
+++ Count all Possible Path.cpp
@@ -0,0 +1,18 @@
+// Eulers cycle or not
+// each node should have even degree
+class Solution {
+    public:
+	int isPossible(vector<vector<int>>paths){
+	    for(int i=0;i<paths.size();i++)
+	    {
+	        int cnt=0;
+	        for(int j=0;j<paths[0].size();j++)
+	        cnt+=paths[i][j];
+	        
+	        if(cnt&1)
+	        return 0;
+	    }
+	    return 1;
+	}
+
+};
