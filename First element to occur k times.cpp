3da197953f2930deb39af7ea1b190e7feff20b9f
Sagar ray <raysagar807@gmail.com>
2024-03-02 09:41:38 +0530

    2nd of march 2024 easy

---
+++ First element to occur k times.cpp
@@ -0,0 +1,16 @@
+// Time complexity - O(N)
+// Space complexity- O(N)
+class Solution{
+    public:
+    int firstElementKTime(int n, int k, int a[])
+    {
+        unordered_map<int,int> mp;
+        for(int i=0;i<n;i++)
+        {
+            mp[a[i]]++;
+            if(mp[a[i]]>=k)
+            return a[i];
+        }
+        return -1;
+    }
+};
