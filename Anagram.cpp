9750ccfa5bc003eeedd3d216d18c1be8b0d68998
Sagar ray <107414907+roy7077@users.noreply.github.com>
2024-11-30 23:27:21 +0530

    30th of nov 2024 easy

---
+++ Anagram.cpp
@@ -0,0 +1,9 @@
+class Solution {
+    public:
+    bool areAnagrams(string& s1, string& s2) {
+        sort(s1.begin(),s1.end());
+        sort(s2.begin(),s2.end());
+        
+        return s1==s2;
+    }
+};
